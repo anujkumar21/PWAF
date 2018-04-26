45bb1cb01665542510fd60ae8233b2ac69171c56
Anuj Kumar <cdac.anuj@gmail.com>
2018-04-26 23:20:52 +0530

    added new test method for selecting option in dropdown list.
    Signed-off-by: Anuj Kumar <cdac.anuj@gmail.com>

--- testcases/dropdown_page_test.py
+++ testcases/dropdown_page_test.py
@@ -10,3 +10,7 @@ class DropdownPageTest(DriverManager):
     def test_dropdown_page(self):
         welcome_page = WelcomePage(self.driver)
         welcome_page.verify_welcome_page().click_on_link("Dropdown")
+
+    def test_select_value_in_dropdown(self):
+        welcome_page = WelcomePage(self.driver)
+        welcome_page.verify_welcome_page().click_on_link("Dropdown").select_dropdown_option("Option 1")
